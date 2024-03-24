b346da3f928a933bfe927c10996330b0ca17fcdc
Kanriver <Kannatee2546kan@gmail.com>
2024-03-24 21:40:36 +0700

    TestOne
    TestOne

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <p class="h1 text-center">Hello</p>
+    <div class="continer">
+        <input type="button" value="Go Login">
+    </div>
+        
+    
+</body>
+</html>
